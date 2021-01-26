5996926d29e186cfa41134cb786b0f1794825ca2
Gisela <galborsz@gmail.com>
2021-01-26 19:13:58 +0100

    changed python code for generating plots

--- score_plot.py
+++ score_plot.py
@@ -1,8 +1,19 @@
 import matplotlib.pyplot as plt
+import sys
 
 iterations = 25000
 
-file1 = open("avg_total_score_sarsa_epsilon_greedy.txt","r")
+#Get input
+algorithm = input("Choose algorithm: qlearning, sarsa, expected_sarsa, double_qlearning\n")
+exploration = input("Choose exploration method: decaying_epsilon_greedy, greedy\n")
+
+print("algorithm: ", algorithm)
+print("exploration: ", exploration)
+
+file_name = "avg_total_score_" + algorithm + "_" + exploration + ".txt"
+title = "Learning progress - " + algorithm + " (" + exploration + ")"
+
+file1 = open(file_name,"r")
 string1 = file1.read().splitlines()
 y = [int(line) for line in string1]
 print("y: ", len(y))
@@ -11,5 +22,5 @@ print("x: ", len(x))
 plt.scatter(x,y, s=0.5)
 plt.xlabel("Iteration")
 plt.ylabel("Score")
-plt.title("Learning progress - Sarsa (epsilon-greedy)")
+plt.title(title)
 plt.show()
